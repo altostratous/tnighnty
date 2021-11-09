3cb66767f3f42bda0c7d45908180ebdaefc9d1a4
Ali Asgari <altostratous@gmail.com>
2021-11-08 19:41:27 -0800

    First version of our state representation

---
+++ src/main/java/representation/State.java
@@ -0,0 +1,22 @@
+package representation;
+
+public class State {
+
+    // The states
+
+    // the relative distance to the enemy (<200, <300, >=300)
+    // our energy (<30, >=30, >100)
+    // the enemy's energy (<30, >=30, >100)
+    // x, y position of our own (step by 100)
+
+    // The actions
+
+    // fire(1)
+    // turn right (90)
+    // turn left (90)
+    // go ahead (100)
+    // go back (100)
+    // do nothing
+
+
+}
