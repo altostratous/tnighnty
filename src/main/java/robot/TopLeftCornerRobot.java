4b440569ab9f13c277967f1dcf59253625954d58
Ali Asgari <altostratous@gmail.com>
2021-11-10 22:28:09 -0800

    Add TopLeftCornerRobot

--- src/main/java/robot/TopLeftCornerRobot.java
+++ src/main/java/robot/TopLeftCornerRobot.java
@@ -1,6 +1,34 @@
 package robot;
 
 import robocode.Robot;
+import robocode.StatusEvent;
 
 public class TopLeftCornerRobot extends Robot {
+
+    @Override
+    public void onStatus(StatusEvent e) {
+        super.onStatus(e);
+        var yDistance = getBattleFieldHeight() - getY();
+        var xDistance = getX();
+        var angle = Math.atan(yDistance / xDistance);
+        angle = 270 + Math.toDegrees(angle);
+        System.out.printf("%f, %f, %f, %f %n", getY(), getX(), getHeading(), angle);
+        double absHeadingDiff = Math.abs(getHeading() - angle);
+        if (getHeading() < angle) {
+            if (absHeadingDiff > 180) {
+                turnLeft(360 - absHeadingDiff);
+            } else {
+                turnRight(absHeadingDiff);
+            }
+        } else {
+            if (absHeadingDiff > 180) {
+                turnRight(360 - absHeadingDiff);
+            } else {
+                turnLeft(absHeadingDiff);
+            }
+        }
+        if (absHeadingDiff < 0.01) {
+            ahead(10000);
+        }
+    }
 }
