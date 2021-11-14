9ee768c9d1e9e20aa29b597d1e092ec82c86c152
Ali Asgari <altostratous@gmail.com>
2021-11-14 10:32:26 -0800

    Add documentation for state representation

--- src/main/java/representation/IStateRepresentation.java
+++ src/main/java/representation/IStateRepresentation.java
@@ -3,5 +3,11 @@ package representation;
 import robocode.Event;
 
 public interface IStateRepresentation extends IRepresentation {
+    /**
+     * Evolves the robot state given the last state and the event
+     * @param state the previous state of the robot
+     * @param event the robot event containing changes to the state
+     * @return returns a new state expressing the changed state
+     */
     IState represent(IState state, Event event);
 }
