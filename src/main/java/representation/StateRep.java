2bf8d6cc924395bd99beb10f57e72224f88cf520
Christina Sun <yipinsun3ch@gmail.com>
2021-11-15 09:24:20 -0800

    StateRep class fixed

--- src/main/java/representation/StateRep.java
+++ src/main/java/representation/StateRep.java
@@ -1,23 +1,33 @@
 package representation;
 import robocode.ScannedRobotEvent;
 import robocode.Event;
+import robocode.StatusEvent;
 
 public class StateRep implements IStateRepresentation {
     public StateRep()  {}
     @Override
-    // can i use this argument event as ScannedRobotEvent? how can i pass in both ScannedRobotEvent and StatusEvent?
-    // Or do i need two classes for two event...
-    public IState represent(IState state, Event enemyEvent) {
-        //why would state be null?
+    // represent method will be called under two circumstances: either from onStatus or from onScannedRobot.
+    // the event passed in can be of either StatusEvent or ScannedRobotEvent
+    public IState represent(IState state, Event event) {
+        //passed states are the last states, all null at first turn
         if (state == null) {
             state = new States(0, 0, 0, 0, 0 ,0);
         }
-        States states = (States) state.clone();
-        if (enemyEvent instanceof ScannedRobotEvent) {
-            ScannedRobotEvent scannedEvent = (ScannedRobotEvent) enemyEvent;
+        States states = (States) state.clone();//cast State to States
+
+        if (event instanceof ScannedRobotEvent) {
+            ScannedRobotEvent scannedEvent = (ScannedRobotEvent) event;
             states.setDistance((int) (scannedEvent.getDistance()));
+            states.setEnemyEnergy((int) scannedEvent.getEnergy()); // does this return energy of the scanned robot or myself?
+        }
+        if (event instanceof StatusEvent) {
+            StatusEvent statusEvent = (StatusEvent) event;
+            states.setX((int) (statusEvent.getStatus().getX() / 100));
+            states.setY((int) (statusEvent.getStatus().getY() / 100));
+            states.setHeading((int) ((statusEvent.getStatus().getHeading() + 45) / 90));
+            states.setEnergy((int) (statusEvent.getStatus().getEnergy()));
         }
         return states;
     }
-//LEFT HERE: TEST IN ROBOCODE, add in enemyEnergy and energy
+
 }
